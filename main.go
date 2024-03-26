3359c16b3625d74b7114ecf2909280e8b6e3116b
swetha.alwaal <swetha.alwaal@paramount.com>
2024-03-25 20:52:52 -0500

    updated

--- main.go
+++ main.go
@@ -1,25 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
+import "net/http"
 
 func main() {
-	fmt.Println("Started service!")
-	http.HandleFunc("/", HelloServer)
-	http.HandleFunc("/index", IndexPage)
-	if errl:=http.ListenAndServe(":8001", nil);errl!=nil{
-		log.Fatalf("Unable to start the server due to :%s",errl.Error())
-	}
-}
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, welcome to, %s!", r.URL.Path[1:])
-}
-func IndexPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Came to index page")
-	w.Header().Add("Content Type", "text/html")
-	http.ServeFile(w, r, "index.html")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+	http.ListenAndServe(":9090", nil)
 }
