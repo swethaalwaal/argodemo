897a1cd4aff00674e69495422061d4d3f4059906
swetha.alwaal <swetha.alwaal@paramount.com>
2024-03-25 21:23:30 -0500

    updated

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Came to handler")
 		w.Write([]byte("Hello World"))
 	})
-	http.ListenAndServe(":9090", nil)
+	fmt.Println("Started serviced on port 8001")
+	http.ListenAndServe(":8001", nil)
 }
