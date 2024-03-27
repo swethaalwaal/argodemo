010f74c381ed145be25b2d10ccaf2af843111fd6
swetha.alwaal <swetha.alwaal@paramount.com>
2024-03-27 11:45:15 -0500

    updated

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM --platform=linux/amd64 golang:1.22
+FROM --platform=linux/arm64/v8 golang:1.22
 WORKDIR /app
 COPY go.mod ./
 RUN go mod download
 COPY *.go ./
+COPY *.html ./
 RUN CGO_ENABLED=0 GOOS=linux go build -o /argodemo
 EXPOSE 8001
 CMD ["/argodemo"]
--- main.go
+++ main.go
@@ -6,11 +6,20 @@ import (
 )
 
 func main() {
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Came to handler")
-		w.Write([]byte("Hello World"))
-	})
 	fmt.Println("Started serviced on port 8001")
+	http.HandleFunc("/health", HealthHandler)
+	http.HandleFunc("/hello", HelloHandler)
+	http.HandleFunc("/index", IndexHandler)
 	http.ListenAndServe(":8001", nil)
+}
+func HealthHandler(w http.ResponseWriter, r *http.Request) {
+	_, _ = w.Write([]byte("Health Check Success!"))
+}
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, welcome to, %s!", r.URL.Path[1:])
+}
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Came to index page")
+	w.Header().Add("Content Type", "text/html")
+	http.ServeFile(w, r, "index.html")
 }
